22cba965600fc4bba17e504bba29dcdfc4ec0918
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-05-29 11:13:02 +0200

    record_and_recognize error handling

--- Aplikacja testowa/SpeechRecognition/speech_recognition.py
+++ Aplikacja testowa/SpeechRecognition/speech_recognition.py
@@ -8,22 +8,24 @@ import RecordingPackage.speech_recognition_recording
 def recognize_speech(AudioData : sr.AudioData, Recognizer : sr.Recognizer):
     r = Recognizer
     audio = AudioData
-    try:
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
-        return r.recognize_google(audio, language="pl-PL")
-    except:
+    return r.recognize_google(audio, language="pl-PL")
 
-        r.recognize_bing()
-    #     print("Google Speech Recognition could not understand audio")
-    # except sr.RequestError as e:
-    #     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
 
 def record_and_recognize():
     """
     Records voice and return AudioData and string with recognized text
     :return Audiodata, str recognized Text
     """
-    AudioData, Recognizer = RecordingPackage.speech_recognition_recording.record_till_end_of_voice()
-    return AudioData, str(recognize_speech(AudioData, Recognizer))
+    while(True):
+        try:
+            AudioData, Recognizer = RecordingPackage.speech_recognition_recording.record_till_end_of_voice()
+            return AudioData, str(recognize_speech(AudioData, Recognizer))
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        except sr.UnknownValueError as e:
+            print("Audio wasn't recognized")
+
