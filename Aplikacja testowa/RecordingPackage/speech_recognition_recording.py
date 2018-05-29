e96d47d745a9d171c3181cdc8319eaede0d96dff
Mikołaj Balcerek <mpbalcerek+github@gmail.com>
2018-05-29 10:48:47 +0200

    Speech Recognition moved to seperate package (#16)

--- Aplikacja testowa/RecordingPackage/speech_recognition_recording.py
+++ Aplikacja testowa/RecordingPackage/speech_recognition_recording.py
@@ -16,28 +16,6 @@ def record_till_end_of_voice():
         audio = r.listen(source)
     return audio, r
 
-
-def recognize_speech(AudioData : sr.AudioData, Recognizer : sr.Recognizer):
-    r = Recognizer
-    audio = AudioData
-    #try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-    return r.recognize_google(audio, language="pl-PL")
-    # except sr.UnknownValueError:
-    #     print("Google Speech Recognition could not understand audio")
-    # except sr.RequestError as e:
-    #     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-def record_and_recognize():
-    """
-    Records voice and return AudioData and string with recognized text
-    :return Audiodata, str recognized Text
-    """
-    AudioData, Recognizer = record_till_end_of_voice()
-    return AudioData, str(recognize_speech(AudioData, Recognizer))
-
 def convert_AudioData_to_Numpy_array_and_fs(AudioData : sr.AudioData):
     """
     this converts AudioData from speech_recognition to Numpy Array and fs int that is used by
