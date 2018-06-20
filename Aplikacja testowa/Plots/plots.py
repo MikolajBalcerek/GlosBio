8314edd8072468a6e55ec9ff4140b9b70676b528
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-06-20 22:03:52 +0200

    init plot module

---
+++ Aplikacja testowa/Plots/plots.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_raw_signal(signal):
+    """ pass """
+    pass
+
+def plot_fft(signal):
+    """ pass """
+    pass
+
+def plot_mel_scale():
+    """ pass """
+    pass
