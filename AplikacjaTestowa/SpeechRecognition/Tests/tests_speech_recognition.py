37f260718086b543d987d6b6908d42f5b6935fa9
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-06-28 16:35:30 +0200

    tests for just text recognition
    FROM ALREADY PREPARED FILE

--- AplikacjaTestowa/SpeechRecognition/Tests/tests_speech_recognition.py
+++ AplikacjaTestowa/SpeechRecognition/Tests/tests_speech_recognition.py
@@ -12,11 +12,16 @@ class TestRecordingAndRecognition(unittest.TestCase):
         self.AUDIO_Mikolaj_Balcerek = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Mikolaj_Balcerek_CLEAR.flac")
         self.AUDIO_Robert_Lewandowski = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Robert_Lewandowski_NOISE.flac")
 
-    def test_smart_recording_and_recognition_clear(self):
+    #tests just for speech recognition in Polish, not recording and noise detection
+    def test_recognize_speech_google_bing_CLEAR_POLISH(self):
         r = sr.Recognizer()
         with sr.AudioFile(self.AUDIO_Mikolaj_Balcerek) as source: audio = r.record(source)
-        self.assertIn([wrapper.record_and_recognize()], "Mikołaj Balcerek")
+        self.assertIn("Mikołaj Balcerek", [wrapper.recognize_speech(audio, r)], "Failed test to recognize speech \"Mikołaj Balcerek\" in a clear ALREADY PREPARED recording")
 
+    def test_recognize_speech_google_bing_NOISE_POLISH(self):
+        r = sr.Recognizer()
+        with sr.AudioFile(self.AUDIO_Robert_Lewandowski) as source: audio = r.record(source)
+        self.assertIn("Robert Lewandowski", [wrapper.recognize_speech(audio, r)], "Failed test to recognize speech \"Rober Lewandowski\" in a noisy ALREADY PREPARED recording")
 
 
 if __name__ == '__main__':
