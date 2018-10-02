e6ed8c477dc5723d20b035052ef95ab37a1d04eb
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-10-02 23:08:55 +0200

    /audio/train endpoint stub

--- AlgorithmAnalyzer/Backend/main.py
+++ AlgorithmAnalyzer/Backend/main.py
@@ -1,55 +1,77 @@
-from flask import request, url_for
+from flask import request, url_for, redirect, Flask
 from flask_api import FlaskAPI, status, exceptions
+from werkzeug.utils import secure_filename
 
+import os
+
+UPLOAD_TRAIN_PATH  = './train'
+ALLOWED_AUDIO_EXTENSIONS = set(['wav', 'mp3'])
 app = FlaskAPI(__name__)
 
 
-notes = {
-    0: 'do the shopping',
-    1: 'build the codez',
-    2: 'paint the door',
-}
-
-def note_repr(key):
-    return {
-        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'text': notes[key]
-    }
+@app.route("/", methods=['GET'])
+def landing_documentation_page():
+    """ Landing page for browsable API """
+    if request.method == 'GET':
+        return ["go to specific /endpoint_name to see documentation"]
 
 
-@app.route("/", methods=['GET', 'POST'])
-def notes_list():
-    """
-    List or create notes.
-    """
+@app.route("/audio/train", methods=['GET', 'POST'])
+def handling_audio_train_endpoint():
+    """ This handles generic operations that have to do with audio
+    being sent/received from test files
+
+    POST to send a new audio file
+    GET to get a list of existing files """
+    def allowed_file(name):
+        """ some function to see if a name could be used as a file name"""
+        return '.' in name and \
+               name.rsplit('.', 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
+
+    if request.method == 'GET':
+        # TODO: return all audio train list
+        return ["hellothere"]
+
     if request.method == 'POST':
-        note = str(request.data.get('text', ''))
-        idx = max(notes.keys()) + 1
-        notes[idx] = note
-        return note_repr(idx), status.HTTP_201_CREATED
+        if 'file' not in request.files:
+            return ['No file part'], status.HTTP_400_BAD_REQUEST
+        if 'username' not in request.data:
+            return ['No username'], status.HTTP_400_BAD_REQUEST
 
-    # request.method == 'GET'
-    return [note_repr(idx) for idx in sorted(notes.keys())]
+        username = request.data.get('username')
+        file = request.files['file']
+
+        #TODO zapisywanie pliku do folderu username/cos
+        final_file_name = ''
+        # if file and allowed_file(file.filename): # and username is a secure string
+        #     # final_file_name = /
+        #     # filename = secure_filename(jakasnazwastring)
+        #     # file.save(os.path.join(app.config['UPLOAD_TRAIN_PATH'], filename))
+        #     pass
+
+        return {"username" : username,
+                "text": f"Uploaded file for {username},"
+                        f" of name {final_file_name}"}, status.HTTP_201_CREATED
 
 
-@app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
-def notes_detail(key):
-    """
-    Retrieve, update or delete note instances.
-    """
-    if request.method == 'PUT':
-        note = str(request.data.get('text', ''))
-        notes[key] = note
-        return note_repr(key)
-
-    elif request.method == 'DELETE':
-        notes.pop(key, None)
-        return '', status.HTTP_204_NO_CONTENT
-
-    # request.method == 'GET'
-    if key not in notes:
-        raise exceptions.NotFound()
-    return note_repr(key)
+# @app.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
+# def notes_detail(key):
+#     """
+#     Retrieve, update or delete note instances.
+#     """
+#     if request.method == 'PUT':
+#         note = str(request.data.get('text', ''))
+#         notes[key] = note
+#         return note_repr(key)
+#
+#     elif request.method == 'DELETE':
+#         notes.pop(key, None)
+#         return '', status.HTTP_204_NO_CONTENT
+#
+#     # request.method == 'GET'
+#     if key not in notes:
+#         raise exceptions.NotFound()
+#     return note_repr(key)
 
 
 if __name__ == "__main__":
