94ae99c836af9c7cba59207d42169675cca96c72
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-10-25 20:25:21 +0200

    change print-logs to flask app logger

--- AlgorithmAnalyzer/Backend/main.py
+++ AlgorithmAnalyzer/Backend/main.py
@@ -9,9 +9,10 @@ from utils import SampleManager, UsernameException
 from speech_recognition_wrapper import speech_to_text_wrapper
 from convert_audio_wrapper import convert_webm
 
-UPLOAD_TRAIN_PATH = './train'
+SAMPLE_UPLOAD_PATH = './data'
 ALLOWED_AUDIO_EXTENSIONS = set(['wav', 'flac', 'webm'])
 app = FlaskAPI(__name__)
+sample_manager = SampleManager(SAMPLE_UPLOAD_PATH)
 
 CORS(app)
 
@@ -43,55 +44,68 @@ def landing_documentation_page():
         return list_routes()
 
 
-@app.route("/audio/train", methods=['GET', 'POST'])
-def handling_audio_train_endpoint():
-    """ This handles generic operations that have to do with audio
+@app.route("/audio/<type>", methods=['GET', 'POST'])
+def handling_audio_endpoint(type):
+    """
+    This handles generic operations that have to do with audio
     being sent/received from test files
 
     POST to send a new audio file
-    GET to get a list of existing files """
+    GET to get a list of registered users
+    """
 
     def allowed_file(name):
         """ some function to see if a name could be used as a file name"""
         return '.' in name and \
                name.rsplit('.', 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
 
+    if type not in ['train', 'test']:
+        return ["Unexpected type '{}' requested".foramt(type)], status.HTTP_400_BAD_REQUEST
+
     if request.method == 'GET':
-        # TODO: return all audio train list
-        return ["hellothere"]
+        return ["hello"], 200
+        # return sample_manager.get_all_usernames()
 
     if request.method == 'POST':
         if 'file' not in request.files:
             return ['No file part'], status.HTTP_400_BAD_REQUEST
         if 'username' not in request.data:
             return ['No username'], status.HTTP_400_BAD_REQUEST
-
+        app.logger.info("new sample to {} set".format(type))
         username = request.data.get('username')
         file = request.files.get('file')
 
         # TO DO: sprawdzanie czy FileStorage zawiera mime type z ALLOWED_AUDIO_EXTENSIONS
-        sample_manager = SampleManager(UPLOAD_TRAIN_PATH)
         try:
             path = sample_manager.save_new_sample(username, file)
-            print("#LOG: .webm file saved to: " + path)
+            app.logger.info(".webm file saved to: {}".format(path))
         except UsernameException:
             return ['Bad username'], status.HTTP_400_BAD_REQUEST
 
         # TO DO: zawinąć konwerter w try - catch
         convert_webm.convert_webm_to_format(
             path, path.replace(".webm", ""), "wav")
-        print("#LOG: file copy converted to wav")
+        app.logger.info("file copy converted to wav")
 
         with sr.AudioFile(path.replace(".webm", ".wav")) as converted_file:
             recognized_speech = speech_to_text_wrapper.recognize_speech(
                 converted_file)
-            print(f"#LOG Recognized words: {recognized_speech}")
+            app.logger.info(f'Recognized words: "{recognized_speech}"')
 
         return {"username": username,
                 "text": f"Uploaded file for {username}, "
                         f"recognized: {recognized_speech}",
                 "recognized_speech": str(recognized_speech)
-        }, status.HTTP_201_CREATED
+                }, status.HTTP_201_CREATED
+
+
+@app.route("/audio/<type>/<user>", methods=['GET'], defaults={'type': 'train'})
+def handle_list_samples_for_user(type, user):
+    """
+    it handles request for listing samples from train or test set
+    for particular user
+    """
+    return ["Get samples for {} from {} set".format(user, type)], 201
 
 
 if __name__ == "__main__":
