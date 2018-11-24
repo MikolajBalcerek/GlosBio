7da4f480b1ab4501610df0788d34ba062361853b
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-11-24 21:46:53 +0100

    Add png and exception tests for mfcc plotting
    Checks for successful MFCC png plot creation
    Checks for thrown exception for saving the plot in a weird file format

--- AlgorithmAnalyzer/Backend/plots/tests/tests_mfcc_plot.py
+++ AlgorithmAnalyzer/Backend/plots/tests/tests_mfcc_plot.py
@@ -6,11 +6,12 @@ from plots.mfcc_plot import _plot_mfcc_color_boxes, plot_save_mfcc_color_boxes
 
 class TestMfccPlotting(unittest.TestCase):
     def setUp(self):
-
         self.AUDIO_1_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "1.wav")
         self.DIRECTORY_TEST = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
-    # unit test for plotting and saving figure
+    # unit tests for plotting and saving figure
+
+    # for pdf plot
     def test_plot_save_mfcc_color_boxed_pdf_success(self):
         # create a plot from test audio and save it
         plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
@@ -26,14 +27,30 @@ class TestMfccPlotting(unittest.TestCase):
         # clear up the test file
         os.remove(test_plot_path)
 
+    # for png plot
+    def test_plot_save_mfcc_color_boxed_success_png(self):
+        # create a plot from test audio and save it
+        plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
+                                   "test_plot_mfcc_png", "png")
 
-    # def test_plot_save_mfcc_color_boxed_success_png(self):
-    #     plot_save_mfcc_color_boxes(self.DIRECTORY_TEST, "test_mfcc_plot", "pdf")
-    #
-    #
-    # def test_plot_save_mfcc_color_boxed_failure_extension(self):
-    #     plot_save_mfcc_color_boxes(self.DIRECTORY_TEST, "test_mfcc_plot", "pdf")
-    #
+        test_plot_path = f"{self.DIRECTORY_TEST}/test_plot_mfcc_png.png"
+        good_plot_path = f"{self.DIRECTORY_TEST}/plot_mfcc_png.png"
+
+        # test assertion contents
+        self.assertEqual(os.path.getsize(test_plot_path), os.path.getsize(good_plot_path),
+                         "Generated MFCC plot PDF file size differs from a known good one")
+
+        # clear up the test file
+        os.remove(test_plot_path)
+
+    # check for exception for bad extension
+    def test_plot_save_mfcc_color_boxed_fail_wrong_extension(self):
+        with self.assertRaises(ValueError, msg="No or bad exception thrown"
+                                               "for .wav output for a mfcc plot"):
+            plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
+                                   "test_plot_mfcc_wav", "wav")
+
+
 
 if __name__ == '__main__':
     unittest.main()
