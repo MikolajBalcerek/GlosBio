c5e5ceea202f1a0d62eedca3797727a69f8f6b4f
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-11-24 20:01:08 +0100

    Working data_boxes mfcc plotting

--- AlgorithmAnalyzer/Backend/plots/mfcc_plot.py
+++ AlgorithmAnalyzer/Backend/plots/mfcc_plot.py
@@ -4,19 +4,37 @@ from matplotlib import cm
 import scipy.io.wavfile as wav
 from python_speech_features import mfcc
 
-# this plots MFCC
-self, username, set_type="train", filetype="wav", no_file_type=False
-def plot_mfcc(username, set_type, filetype):
-    (rate,sig) = wav.read("1.wav")
-    mfcc_feat = mfcc(sig,rate)
+# this file handles plotting and saving mfcc plots
 
-    ig, ax = plt.subplots()
-    mfcc_data= np.swapaxes(mfcc_feat, 0 ,1)
-    cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower', aspect='auto')
-    ax.set_title('MFCC')
-    #Showing mfcc_data
-    plt.show()
-    #Showing mfcc_feat
-    plt.plot(mfcc_feat)
-    plt.show()
 
+def plot_mfcc_feat_lines():
+    (rate, sig) = wav.read("1.wav")
+    mfcc_feat = mfcc(sig, rate)
+    fig = plt.figure(figsize=(10,10))
+    return fig
+
+
+def plot_mfcc_data_boxes():
+    (rate, sig) = wav.read("1.wav")
+    mfcc_feat = mfcc(sig, rate)
+    fig, ax = plt.subplots()
+    mfcc_data = np.swapaxes(mfcc_feat, 0, 1)
+    cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm,
+                    origin='lower', aspect='auto')
+
+    return fig
+
+
+
+def save_matplotlib_plot(data, title=None, saved_format: str="pdf"):
+    """
+
+    :param size_inches: int size in inches of the plot to be created, 5 -> 5x5
+    :param saved_format: str type of plot image to be saved, png or pdf
+    :return:
+    """
+    data.savefig(f'fo474o.png', transparent= True, bbox_inches='tight')
+
+if __name__ == "__main__":
+    mfcc_data = plot_mfcc_data_boxes();
+    save_matplotlib_plot(mfcc_data)
