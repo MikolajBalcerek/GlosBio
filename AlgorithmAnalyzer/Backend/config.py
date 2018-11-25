1858aef6d9b4c89942b446d43833db10c97fc6c0
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-11-25 02:01:19 +0100

    Add plots in config

--- AlgorithmAnalyzer/Backend/config.py
+++ AlgorithmAnalyzer/Backend/config.py
@@ -1,9 +1,10 @@
 # directory where samples will be stored
 SAMPLE_UPLOAD_PATH = './data'
 
-# avaliable classes of samples
+# available classes of samples
 ALLOWED_SAMPLE_TYPES = ['train', 'test']
 
-# avaliable filetypes and corresponding allowed extensions
+# available filetypes and corresponding allowed extensions
 ALLOWED_FILES_TO_GET = {'audio': ['wav', 'webm'],
-                        'json': ['json']}
+                        'json': ['json'],
+                        'plot': ['pdf', 'png']}
