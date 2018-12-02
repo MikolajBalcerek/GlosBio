48f5806dda83695bb56e4ffae3972b7968078897
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-12-02 18:16:44 +0100

    change config

--- AlgorithmAnalyzer/Backend/config.py
+++ AlgorithmAnalyzer/Backend/config.py
@@ -7,3 +7,8 @@ ALLOWED_SAMPLE_TYPES = ['train', 'test']
 # avaliable filetypes and corresponding allowed extensions
 ALLOWED_FILES_TO_GET = {'audio': ['wav', 'webm'],
                         'json': ['json']}
+
+# MongoDB database settings
+DATABASE_URL = "127.0.0.1"
+DATABASE_PORT = "27018"
+DATABASE_NAME = "samplebase"
