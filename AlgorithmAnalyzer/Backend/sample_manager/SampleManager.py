9c1f35958aaa6cb80e65a5fcb60fe7459a850615
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-12-09 15:51:41 +0100

    comments

--- AlgorithmAnalyzer/Backend/sample_manager/SampleManager.py
+++ AlgorithmAnalyzer/Backend/sample_manager/SampleManager.py
@@ -9,10 +9,12 @@ import pprint
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 from mimetypes import guess_type
+from werkzeug.wsgi import wrap_file
 
 # from scipy.io import wavfile
 # from pathlib import Path
 from pymongo import MongoClient, errors
+from bson.objectid import ObjectId
 import gridfs
 import datetime
 
@@ -24,8 +26,32 @@ from utils.speech_recognition_wrapper import speech_to_text_wrapper
 example of single MongoDB document representing single sample class
 
 {
-
-
+	"_id" : ObjectId("5c05b2a837aeab2bca848c74"),
+	"name" : "Test Test",
+	"name-normalized" : "test_test",
+	"created" : ISODate("2018-12-03T22:48:08.449Z"),
+	"samples" : {
+		"train" : [
+			{
+				"filename" : "1.wav",
+				"id" : ObjectId("5c05b2a837aeab2bca848c75")
+			},
+			{
+				"filename" : "2.wav",
+				"id" : ObjectId("5c05b7ae37aeab2f3e779659")
+			},
+			{
+				"filename" : "3.wav",
+				"id" : ObjectId("5c05c17c37aeab3cf2e5cb76")
+			},
+			{
+				"filename" : "4.wav",
+				"id" : ObjectId("5c05c19837aeab3cf2e5cb78")
+			}
+		],
+		"test" : [ ]
+	},
+	"tags" : [ ]
 }
 
 '''''''''''''''
@@ -34,12 +60,10 @@ example of single MongoDB document representing single sample class
 class SampleManager:
     def __init__(self, db_url: str, db_name: str):
         '''
-        setup MongoDB database connetion
-
-        :param db_url: url to MongioDB database
+        :param db_url: url to MongioDB database, it can contain port eg: "localhost:27017"
         :param db_name: database name
         '''
-
+        # setup MongoDB database connetion
         self.db_client = MongoClient(db_url)
         self.db_database = self.db_client[db_name]
         self.db_collection = self.db_database.samples
@@ -65,22 +89,26 @@ class SampleManager:
 
     def user_exists(self, username: str) -> bool:
         '''
-        check if user already exists in samplebase
+        check if user already exists in samplebas
+        :param username: str - eg. "Hugo Kołątaj"
         '''
-        norm_name = self._username_to_dirname(username)
+        norm_name = self._get_normalized_username(username)
         return True if self.db_collection.find_one({"name-normalized": norm_name}) else False
 
-    def sample_exists(self, username, type, samplename):
-        # TO DO: reimplement
-        pass
-        # dir = self.get_user_dirpath(username)
-        # if type == 'test':
-        #     dir = os.path.join(dir, type)
-        # return Path(os.path.join(dir, sample)).exists()
+    def sample_exists(self, username: str, set_type: str, samplename: str) -> bool:
+        '''
+        check if sample exists in samplebase
+        :param username: str - eg. "Hugo Kołątaj"
+        :param set_type: str - one of avalible sample classes from config
+        :param samplename str - eg. "1.wav"
+        '''
+        sample_list = self.get_user_sample_list(username, set_type)
+        return samplename in sample_list
 
-    def create_user(self, username: str):
+    def create_user(self, username: str) -> ObjectId:
         '''
         create new user in samplebase
+        :param username: str - eg. "Hugo Kołątaj"
         '''
         new_saple = self._get_sample_class_document_template(username)
         id = self.db_collection.insert_one(new_saple).inserted_id
@@ -89,8 +117,8 @@ class SampleManager:
     def get_user_sample_list(self, username: str, set_type: str) -> list:
         '''
         get list of files from particular user
-        :param username: str
-        :param set_type: str - 'train' or 'test'
+        :param username: str - eg. "Hugo Kołątaj"
+        :param set_type: str - one of avalible sample classes from config
         '''
         id = self._get_user_mongo_id(username)
         doc = self.db_collection.find_one({'_id': id}, {f"samples.{set_type}.filename": 1, '_id': 0})
@@ -102,36 +130,15 @@ class SampleManager:
             sample_names.append(sample['filename'])
         return sample_names
 
-    # @staticmethod
-    # def get_new_json_path(audio_path: str) -> str:
-    #     """ this gets path for a new recording's json file
-    #      based on str audio_path
-    #      e.g C:/aasda/a.wav -> C:/aasda/a.json"""
-    #     return audio_path.rsplit('.', 1)[0]+".json"
-
-    # def get_sample_file(self, username, type, sample):
-        # pass
-        # username = self.username_to_dirname(username)
-        # sample_path = os.path.join(
-        #     self.path, username,
-        #     '{}.wav'.format(sample_number)
-        # )
-        # return wavfile.read(sample_path)
-
-    # @staticmethod
-    # def is_allowed_file_extension(file: FileStorage) -> bool:
-    #     """
-    #     checks whether mimetype of the file is allowed
-    #     :param file: FileStorage
-    #     :return: True/False
-    #     """
-    #     return True if file.mimetype == "audio/wav" else False
-
     def save_new_sample(self, username: str, set_type: str, file: FileStorage) -> str:
         '''
-
+        saves new sample in samplebase, creates new user if
+        it wasn't created yet
+        :param username: str - eg. "Hugo Kołątaj"
+        :param set_type: str - one of avalible sample classes from config
+        :param file: FileStorage
         '''
-
+        # TO DO: add speech to text
         if not self.user_exists(username):
             self.create_user(username)
 
@@ -148,79 +155,44 @@ class SampleManager:
 
         return ""
 
-    def get_samplefile(self, username: str, type: str, filename: str):
+    def get_samplefile(self, username: str, set_type: str, samplename: str):
         '''
-
+        retrive sample from database, return as file-like object (with read() method)
+        :param username: str - eg. "Hugo Kołątaj"
+        :param set_type: str - one of avalible sample classes from config
+        :param samplename str - eg. "1.wav"
         '''
-        id = self.db_collection.find_one({'_id': self._get_user_mongo_id(username)}, ["samples.train"])["samples"]["train"][0]
-        out = self.db_file_storage.get(id)
-        print(out.read())
-        out_2 = FileStorage(stream=out.read(), filename="1.wav", content_type="audio/wav")
-        return out_2
-        
-        # if not self.user_exists(username):
-        #     self.create_user(username)
+        user_id = self._get_user_mongo_id(username)
 
-        # if self.is_allowed_file_extension(file):
-        #     wav_path = self.get_new_sample_path(username, set_type=set_type, filetype="wav")
-        #     file.save(wav_path)
-        #     print(f"#LOG {self.__class__.__name__}: .wav file saved to: " + wav_path)
-        # else:
-        #     # not-wav file is temporarily saved
-        #     temp_path = self.get_new_sample_path(username, set_type=set_type, no_file_type=True)
-        #     print()
-        #     file.save(temp_path)
+        # aggregation query to find file id
+        aggregation_pipeline = [
+            {'$match': {'_id': user_id}},
+            {'$project': {f"samples.{set_type}": 1, '_id': 0}},
+            {'$unwind': f"$samples.{set_type}"},
+            {'$match': {f"samples.{set_type}.filename": samplename}},
+            {'$project': {"id": f"$samples.{set_type}.id"}}
+            ]
 
-        #     # convert to webm
-        #     wav_path = convert_webm.convert_webm_to_format(
-        #         temp_path, temp_path,  "wav")
-        #     print("#LOG {self.__class__.__name__}: .wav file converted and saved to: " + wav_path)
+        temp_doc = list(self.db_collection.aggregate(aggregation_pipeline))
+        if not temp_doc:
+            return None
+        id = list(temp_doc)[0]['id']
+        print(id)
+        fileObj = self.db_file_storage.get(id)
+        return fileObj
 
-        #     # delete temp file
-        #     os.remove(temp_path)
-
-        # # recognize speech
-        # recognized_speech = speech_to_text_wrapper.recognize_speech_from_path(wav_path)
-        # print(f"#LOG {self.__class__.__name__}: Recognized words: {recognized_speech}")
-
-        # # save the new sample json
-        # json_path = self.create_a_new_sample_properties_json(username, audio_path=wav_path,
-        #                                                      data={"recognized_speech": recognized_speech})
-        # print(f"#LOG {self.__class__.__name__}: Created a JSON file: {json_path}")
-
-        # return wav_path, recognized_speech
-
-    # @staticmethod
-    # def create_a_new_sample_properties_json(username, data: typing.Dict[str, str], audio_path: str) -> str:
-    #     """
-    #     this creates a json file for the newest sample for the username given
-    #     e.g: 5.json
-    #     takes data to be saved in the form of dict[string, string]
-    #     audio_path is a path to the audio file to be accompanied by json file
-    #     :param username: str
-    #     :param data: typing.Dict[str, str]
-    #     :param audio_path: str path to audio
-    #     :return: str path to json
-    #     """
-    #     json_path = SampleManager.get_new_json_path(audio_path)
-    #     with open(json_path, 'w', encoding='utf8') as json_file:
-    #         recording_properties = {"name": username, **data}
-    #         string_json = str(json.dumps(recording_properties, ensure_ascii=False).encode('utf8'), encoding='utf8')
-    #         json_file.writelines(string_json)
-    #     return json_path
-
-    # def is_wav_file(self, samplename):
-    #     return re.match('.+\.wav$', samplename)
     def _invalid_username(self, username: str) -> bool:
         '''
         check if given username is valid
         '''
         return not re.match('^\w+$', username)
-    
-    def _username_to_dirname(self, username: str) -> str:
+
+    def _get_normalized_username(self, username: str) -> str:
         '''
-        Convert username, which could include spaces,
-        big letters and diacritical marks, to valid directory name
+        Normalize username, which could include spaces,
+        big letters and diacritical marks
+        example: "Hugo Kołątaj" --> "hugo_kolataj"
+        :param username: str - eg. "Hugo Kołątaj"
         '''
 
         temp = username.lower()
@@ -250,27 +222,27 @@ class SampleManager:
         need for 'fresh' document templete, eg. creating new user
         '''
         return {"name": username,
-                "name-normalized": self._username_to_dirname(username),
+                "name-normalized": self._get_normalized_username(username),
                 "created": datetime.datetime.utcnow(),
                 "samples": {"train": [], "test": []},
                 "tags": []
                 }
 
-    def _get_sample_file_document_template(self, filename, id) -> dict:
+    def _get_sample_file_document_template(self, filename, id: DocObjectId) -> dict:
         '''
         get single file document template
         '''
         return {"filename": filename, "id": id}
 
-    def _get_user_mongo_id(self, username: str):
+    def _get_user_mongo_id(self, username: str) -> DocObjectId:
         '''
         needed when we want to refere to db document via mongo _id
         and have username
         '''
-        doc = self.db_collection.find_one({"name-normalized": self._username_to_dirname(username)})
+        doc = self.db_collection.find_one({"name-normalized": self._get_normalized_username(username)})
         return doc['_id'] if doc else None
 
-    def _save_file_to_db(self, filename: str = 'test.wav', fileObj=None, content_type: str = None):
+    def _save_file_to_db(self, filename: str, fileObj=None, content_type: str = None):
         '''
         save file-like object, eg. FileStorage, to database
         '''
@@ -281,7 +253,7 @@ class SampleManager:
         id = storage.put(fileObj, filename=filename, content_type=content_type)
         return id
 
-    def _get_file_from_db(self, id):
+    def _get_file_from_db(self, id: DocObjectId):
         '''
         get file-like object, eg. FileStorage, from database
         '''
@@ -309,12 +281,13 @@ class SampleManager:
         next_number = int(regex.group(1)) + 1
         return f"{next_number}.wav"
 
+    def _is_wav_file(self, samplename):
+        return re.match('.+\.wav$', samplename)
 
     # def file_has_proper_extension(self, filename: str, allowed_extensions: list) -> typing.Tuple[bool, str]:
     #     """
     #     it takes 'filename' and decide if it has extension which can be found
     #     in 'allowed_extensions'
-      
     #     important: extensions in 'allowed_extensions' should not start with dot, eg:
     #        good: ['webm', 'cat', 'wav']
     #        bad:  ['.webm', '.cat', '.wav']
@@ -331,10 +304,6 @@ class SampleManager:
     #             return True, file_extension
 
 
-
-
-
 class UsernameException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-
--- AlgorithmAnalyzer/Backend/sample_manager/tests.py
+++ AlgorithmAnalyzer/Backend/sample_manager/tests.py
@@ -3,120 +3,127 @@ import unittest
 import shutil
 from pathlib import Path
 
+from pymongo import MongoClient
 from werkzeug.datastructures import FileStorage
 from sample_manager.SampleManager import SampleManager, UsernameException
+
+import config
 
 
 class TestSampleManager(unittest.TestCase):
 
     def setUp(self):
-        self.sample_manager = SampleManager('./test')
-        self.username = "qwe rty"
+        self.db_url = f"{config.DATABASE_URL}:{config.DATABASE_PORT}"
+        self.db_name = f"{config.DATABASE_NAME}_test"
+        self.sample_manager = SampleManager(self.db_url, self.db_name)
 
     def tearDown(self):
-        shutil.rmtree('./test', ignore_errors=True)
+        client = MongoClient(self.db_url)
+        client.drop_database(self.db_name)
+        
+        # shutil.rmtree('./test', ignore_errors=True)
 
-    def test_username_to_dirname_should_pass(self):
-        username = 'Abcd Efgh'
-        dirname = self.sample_manager.username_to_dirname(username)
-        self.assertEqual(dirname, 'abcd_efgh')
+    # def test_username_to_dirname_should_pass(self):
+    #     username = 'Abcd Efgh'
+    #     dirname = self.sample_manager.username_to_dirname(username)
+    #     self.assertEqual(dirname, 'abcd_efgh')
 
-    def test_username_to_dirname_invalid_username(self):
-        username = 'a ?'
-        with self.assertRaises(UsernameException) as ctx:
-            self.sample_manager.username_to_dirname(username)
-        self.assertIn('Incorrect username "a_?" !', str(ctx.exception))
+    # def test_username_to_dirname_invalid_username(self):
+    #     username = 'a ?'
+    #     with self.assertRaises(UsernameException) as ctx:
+    #         self.sample_manager.username_to_dirname(username)
+    #     self.assertIn('Incorrect username "a_?" !', str(ctx.exception))
 
-    def test_create_user_should_pass(self):
-        self.sample_manager.create_user(self.username)
+    # def test_create_user_should_pass(self):
+    #     self.sample_manager.create_user(self.username)
 
-    def test_create_users_should_pass(self):
-        uname1 = "asd asd"
-        uname2 = "dasd dasd"
-        self.sample_manager.create_user(uname1)
-        self.sample_manager.create_user(uname1)
-        self.sample_manager.create_user(uname2)
+    # def test_create_users_should_pass(self):
+    #     uname1 = "asd asd"
+    #     uname2 = "dasd dasd"
+    #     self.sample_manager.create_user(uname1)
+    #     self.sample_manager.create_user(uname1)
+    #     self.sample_manager.create_user(uname2)
 
-    def test_user_exists(self):
-        no = self.sample_manager.user_exists(self.username)
-        self.assertFalse(no)
-        self.sample_manager.create_user(self.username)
-        yes = self.sample_manager.user_exists(self.username)
-        self.assertTrue(yes)
+    # def test_user_exists(self):
+    #     no = self.sample_manager.user_exists(self.username)
+    #     self.assertFalse(no)
+    #     self.sample_manager.create_user(self.username)
+    #     yes = self.sample_manager.user_exists(self.username)
+    #     self.assertTrue(yes)
 
-    def test_get_all_usernames(self):
-        uname1 = "Aud asd"
-        uname2 = "śćą ÜüŹąłä"
-        unames = [uname1, uname2, self.username]
-        for uname in unames:
-            self.sample_manager.create_user(uname)
-        dirnames = set(['aud_asd', 'sca_uuzala', 'qwe_rty'])
-        self.assertEqual(dirnames, set(self.sample_manager.get_all_usernames()))
+    # def test_get_all_usernames(self):
+    #     uname1 = "Aud asd"
+    #     uname2 = "śćą ÜüŹąłä"
+    #     unames = [uname1, uname2, self.username]
+    #     for uname in unames:
+    #         self.sample_manager.create_user(uname)
+    #     dirnames = set(['aud_asd', 'sca_uuzala', 'qwe_rty'])
+    #     self.assertEqual(dirnames, set(self.sample_manager.get_all_usernames()))
 
-    def test_add_sample_without_user_should_not_pass(self):
-        sample = b'aaaaaaaa'
-        with self.assertRaises(FileNotFoundError):
-            self.sample_manager.add_sample(self.username, sample)
+    # def test_add_sample_without_user_should_not_pass(self):
+    #     sample = b'aaaaaaaa'
+    #     with self.assertRaises(FileNotFoundError):
+    #         self.sample_manager.add_sample(self.username, sample)
 
-    def test_add_sample_should_pass(self):
-        sample = b'aaaaaa'
-        self.sample_manager.create_user(self.username)
-        self.sample_manager.add_sample(self.username, sample)
+    # def test_add_sample_should_pass(self):
+    #     sample = b'aaaaaa'
+    #     self.sample_manager.create_user(self.username)
+    #     self.sample_manager.add_sample(self.username, sample)
 
-    def test_get_samples(self):
-        self.sample_manager.create_user(self.username)
-        self.assertEqual([], self.sample_manager.get_samples(self.username))
-        samples = [b'asdasd', b'bbbbb', b'cccc']
-        for sample in samples:
-            self.sample_manager.add_sample(self.username, sample)
-        self.assertEqual(
-            ['1.wav', '2.wav', '3.wav'],
-            self.sample_manager.get_samples(self.username)
-        )
+    # def test_get_samples(self):
+    #     self.sample_manager.create_user(self.username)
+    #     self.assertEqual([], self.sample_manager.get_samples(self.username))
+    #     samples = [b'asdasd', b'bbbbb', b'cccc']
+    #     for sample in samples:
+    #         self.sample_manager.add_sample(self.username, sample)
+    #     self.assertEqual(
+    #         ['1.wav', '2.wav', '3.wav'],
+    #         self.sample_manager.get_samples(self.username)
+    #     )
 
-    def test_get_sample_should_pass(self):
-        pass
+    # def test_get_sample_should_pass(self):
+    #     pass
 
-    def test_get_json_path(self):
-        example_path_wav = "C:/a.wav"
-        example_path_webm = "/home/train/5.webm"
+    # def test_get_json_path(self):
+    #     example_path_wav = "C:/a.wav"
+    #     example_path_webm = "/home/train/5.webm"
 
-        suggested_json_path_wav = self.sample_manager.get_new_json_path(
-            audio_path=example_path_wav)
-        self.assertEqual(suggested_json_path_wav, "C:/a.json")
+    #     suggested_json_path_wav = self.sample_manager.get_new_json_path(
+    #         audio_path=example_path_wav)
+    #     self.assertEqual(suggested_json_path_wav, "C:/a.json")
 
-        suggested_json_path_wav = self.sample_manager.get_new_json_path(
-            audio_path=example_path_webm)
-        self.assertEqual(suggested_json_path_wav, "/home/train/5.json")
+    #     suggested_json_path_wav = self.sample_manager.get_new_json_path(
+    #         audio_path=example_path_webm)
+    #     self.assertEqual(suggested_json_path_wav, "/home/train/5.json")
 
-    def test_create_json_with_content(self):
-        """ test for creating a new sample properties json"""
-        self.sample_manager.create_user("Mikołaj Balcerek")
-        json_Path = Path(SampleManager.create_a_new_sample_properties_json("Mikołaj Balcerek",
-                                                          {"recognized_speech": "test"},
-                                                        self.sample_manager.path+"/mikolaj_balcerek/1.wav"))
+    # def test_create_json_with_content(self):
+    #     """ test for creating a new sample properties json"""
+    #     self.sample_manager.create_user("Mikołaj Balcerek")
+    #     json_Path = Path(SampleManager.create_a_new_sample_properties_json("Mikołaj Balcerek",
+    #                                                       {"recognized_speech": "test"},
+    #                                                     self.sample_manager.path+"/mikolaj_balcerek/1.wav"))
 
-        self.assertTrue(json_Path.exists(), "Example JSON file was not created")
+    #     self.assertTrue(json_Path.exists(), "Example JSON file was not created")
 
-        with json_Path.open(encoding='utf8') as _json_file:
-            json_dict = json.loads(_json_file.read(), encoding='utf8')
-            self.assertIn(json_dict["recognized_speech"],
-                          ["test", 13, '13'],
-                          "incorrect recognized_speech in JSON")
+    #     with json_Path.open(encoding='utf8') as _json_file:
+    #         json_dict = json.loads(_json_file.read(), encoding='utf8')
+    #         self.assertIn(json_dict["recognized_speech"],
+    #                       ["test", 13, '13'],
+    #                       "incorrect recognized_speech in JSON")
 
-            self.assertEqual(json_dict["name"], "Mikołaj Balcerek",
-                              "incorrect name in JSON")
+    #         self.assertEqual(json_dict["name"], "Mikołaj Balcerek",
+    #                           "incorrect name in JSON")
 
-    def test_is_extension_allowed_wav(self):
-        file = FileStorage(content_type="audio/wav")
-        self.assertTrue(SampleManager.is_allowed_file_extension(file),
-                        "file with content type audio/wav was not allowed")
+    # def test_is_extension_allowed_wav(self):
+    #     file = FileStorage(content_type="audio/wav")
+    #     self.assertTrue(SampleManager.is_allowed_file_extension(file),
+    #                     "file with content type audio/wav was not allowed")
 
-    def test_is_extension_allowed_webm_disallowed(self):
-        file = FileStorage(content_type="audio/webm")
-        self.assertFalse(SampleManager.is_allowed_file_extension(file),
-                        "file with content type audio/webm was allowed"
-                        " (should fail the test to be later converted)")
+    # def test_is_extension_allowed_webm_disallowed(self):
+    #     file = FileStorage(content_type="audio/webm")
+    #     self.assertFalse(SampleManager.is_allowed_file_extension(file),
+    #                     "file with content type audio/webm was allowed"
+    #                     " (should fail the test to be later converted)")
 
 
 if __name__ == '__main__':
