9e3d2006447fcb60866ff7b5b86798d7597b6a67
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-12-09 15:24:21 +0100

    add send file endpoint

--- AlgorithmAnalyzer/Backend/main.py
+++ AlgorithmAnalyzer/Backend/main.py
@@ -1,9 +1,9 @@
 import urllib
-import os
 
-from flask import request, current_app, send_from_directory
+from flask import request, current_app, send_file
 from flask_api import FlaskAPI, status
 from flask_cors import CORS
+from io import BytesIO
 
 import config
 from sample_manager.SampleManager import SampleManager, UsernameException
@@ -115,43 +115,32 @@ def handle_get_file(sampletype, username, samplename):
     samplename: full name of requested sample eg. '1', '150'
     """
 
-    # check for proper file type
-    # if filetype not in list(config.ALLOWED_FILES_TO_GET.keys()):
-    #     return [f"Unexpected file type '{filetype}' requested.Expected one of: {list(config.ALLOWED_FILES_TO_GET.keys())}"], \
-    #            status.HTTP_400_BAD_REQUEST
-
     # check for proper sample set type
-    # if sampletype not in config.ALLOWED_SAMPLE_TYPES:
-    #     return [f"Unexpected sample type '{sampletype}' requested. Expected one of: {config.ALLOWED_SAMPLE_TYPES}"], \
-    #             status.HTTP_400_BAD_REQUEST
+    if sampletype not in config.ALLOWED_SAMPLE_TYPES:
+        return [f"Unexpected sample type '{sampletype}' requested. Expected one of: {config.ALLOWED_SAMPLE_TYPES}"], \
+                status.HTTP_400_BAD_REQUEST
 
-    # # check if user exists in samplebase
-    # if not sample_manager.user_exists(username):
-    #     return [f"There is no such user '{username}' in sample base"], status.HTTP_400_BAD_REQUEST
+    # check if user exists in samplebase
+    if not sample_manager.user_exists(username):
+        return [f"There is no such user '{username}' in sample base"], status.HTTP_400_BAD_REQUEST
 
-    # # check if requested file have allowed extension
+    # check if requested file have allowed extension
     # allowed_extensions = config.ALLOWED_FILES_TO_GET[filetype]
     # proper_extension, extension = sample_manager.file_has_proper_extension(filename, allowed_extensions)
     # if not proper_extension:
     #     return [f"Accepted extensions for filetype '{filetype}': {allowed_extensions}, but got '{extension}' instead"],\
     #             status.HTTP_400_BAD_REQUEST
 
-    # check if file exists in samplebase
-    # if not sample_manager.file_exists(username, sampletype, filename):
-    #     return [f"There is no such sample '{filename}' in users '{username}' {sampletype} samplebase"],\
-    #             status.HTTP_400_BAD_REQUEST
+    file = sample_manager.get_samplefile(username, sampletype, samplename)
 
-    # serve file
-    # user_dir = sample_manager.get_user_dirpath(username)
-    # if sampletype == 'test':
-    #     user_dir = os.path.join(user_dir, sampletype)
+    # check if file exists in samplebase
+    if not file:
+        return [f"There is no such sample '{samplename}' in users '{username}' {sampletype} samplebase"],\
+                status.HTTP_400_BAD_REQUEST
 
     app.logger.info(f"send file '{samplename}' from database")
-    file = sample_manager.get_samplefile(username, sampletype, samplename)
-    file.save("./1.wav")
-    print(file)
-    # return send_from_directory(user_dir, filename, as_attachment=True), status.HTTP_200_OK
-    return []
+    return send_file(BytesIO(file.read()), mimetype=file.content_type)
+    
 
 
 if __name__ == "__main__":
