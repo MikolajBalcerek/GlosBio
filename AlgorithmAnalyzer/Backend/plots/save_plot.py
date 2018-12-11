a4ec4e52dce0e346d89a9f359186d0cb24809ff2
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-12-11 16:09:00 +0100

    Save_plot returning both path to file and BytesIO object
    To align with expected SampleManager rework

--- AlgorithmAnalyzer/Backend/plots/save_plot.py
+++ AlgorithmAnalyzer/Backend/plots/save_plot.py
@@ -1,10 +1,13 @@
+from io import BytesIO
+from typing import Tuple
+
 import matplotlib.pyplot as plt
 
 # This file holds helper functions that save plots from different packages
 
 
 def save_matplotlib_figure(data: plt.Figure, directory_path: str,
-                           file_name: str, saved_format: str = "png") -> str:
+                           file_name: str, saved_format: str = "png") -> Tuple[str, BytesIO]:
     """
     This function saves a matplotlib Figure object to a file
 
@@ -14,12 +17,20 @@ def save_matplotlib_figure(data: plt.Figure, directory_path: str,
     :param saved_format: str type of plot image to be saved, png or pdf,
     defaults to pdf (vector format)
 
-    :return file_path: str file_path to the saved file
+    :return file_path, file_io: str file_path to the saved file,
+    BytesIO containing the requested plot
     """
     if saved_format == "png" or saved_format == "pdf":
+        # TODO: purge path way after new SampleManager
         file_path = f'{directory_path}/{file_name}.{saved_format}'
         data.savefig(file_path,
                      transparent=True, bbox_inches='tight')
-        return file_path
+
+        # saving in two ways for future SampleManager
+        file_io = BytesIO()
+        data.savefig(file_io, transparent=True, bbox_inches='tight',
+                     format=saved_format)
+
+        return file_path, file_io
     else:
         raise ValueError("Expected png or pdf as saved_format")
