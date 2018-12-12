caa88c50672257c237c3e5ff903cddedcf930ecb
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-12-13 00:41:03 +0100

    Modify new mfcc_plot tests to cover BytesIO cases
    Also fixes small string mistakes in error messages

--- AlgorithmAnalyzer/Backend/plots/tests/tests_mfcc_plot.py
+++ AlgorithmAnalyzer/Backend/plots/tests/tests_mfcc_plot.py
@@ -6,6 +6,8 @@ from plots.mfcc_plot import _plot_mfcc_color_boxes, plot_save_mfcc_color_boxes
 
 
 class TestMfccPlotting(unittest.TestCase):
+
+    @classmethod
     def setUpClass(self):
         self.AUDIO_1_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "1.wav")
         self.DIRECTORY_TEST = os.path.join(os.path.dirname(os.path.realpath(__file__)))
@@ -15,27 +17,40 @@ class TestMfccPlotting(unittest.TestCase):
     # for pdf plot
     def test_plot_save_mfcc_color_boxed_pdf_success(self):
         # create a plot from test audio and save it
-        file_path = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
+        file_path, plot_bytesIO = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
                                    "test_plot_mfcc_pdf", "pdf")
 
         test_plot_path = f"{self.DIRECTORY_TEST}/test_plot_mfcc_pdf.pdf"
         good_plot_path = f"{self.DIRECTORY_TEST}/plot_mfcc_pdf.pdf"
 
-        # test assertion contents
+        # test assertion file_path as expected
+        self.assertEqual(test_plot_path, file_path, "File path for the pdf MFCC"
+                                                    "plot different than expected")
+
+        # test assertion saved file size is the same as expected
         self.assertEqual(os.path.getsize(test_plot_path), os.path.getsize(good_plot_path),
                          "Generated MFCC plot PDF file size differs from a known good one")
 
-        # test assertion file_path as expected
-        self.assertEqual(test_plot_path, file_path, "File path for the png MFCC"
-                                                    "plot different than expected")
+
+        # test assertion in-memory plot when saved is of the same size as expected
+        test_in_memory_plot_path = f"{self.DIRECTORY_TEST}/test_in_memory_plot_mfcc_pdf.pdf"
+        # save in-memory plot to file - decided to do this the following way
+        # as comparing file from memory to saved one results from meta-data bytes differing
+        with open(test_in_memory_plot_path, "wb") as file:
+            file.write(plot_bytesIO.getvalue())
+
+        self.assertEqual(os.path.getsize(good_plot_path),
+                         os.path.getsize(test_in_memory_plot_path),
+                         "Generated MFCC plot PDF file from memory when saved differs in size from a known good one")
 
         # clear up the test file
         os.remove(test_plot_path)
+        os.remove(test_in_memory_plot_path)
 
     # for png plot
     def test_plot_save_mfcc_color_boxed_success_png(self):
         # create a plot from test audio and save it
-        file_path = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
+        file_path, plot_bytesIO = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH, self.DIRECTORY_TEST,
                                    "test_plot_mfcc_png", "png")
 
         test_plot_path = f"{self.DIRECTORY_TEST}/test_plot_mfcc_png.png"
@@ -43,15 +58,27 @@ class TestMfccPlotting(unittest.TestCase):
 
         # test assertion contents
         self.assertEqual(os.path.getsize(test_plot_path), os.path.getsize(good_plot_path),
-                         "Generated MFCC plot PDF file size differs from a known good one")
+                         "Generated MFCC plot PNG file size differs from a known good one")
 
         # test assertion file_path as expected
         self.assertEqual(test_plot_path, file_path, "File path for the png MFCC"
                                                     "plot different than expected")
 
 
+        # test assertion in-memory plot when saved is of the same size as expected
+        test_in_memory_plot_path = f"{self.DIRECTORY_TEST}/test_in_memory_plot_mfcc_png.png"
+        # save in-memory plot to file - decided to do this the following way
+        # as comparing file from memory to saved one results from meta-data bytes differing
+        with open(test_in_memory_plot_path, "wb") as file:
+            file.write(plot_bytesIO.getvalue())
+
+        self.assertEqual(os.path.getsize(good_plot_path),
+                         os.path.getsize(test_in_memory_plot_path),
+                         "Generated MFCC plot PNG file from memory when saved differs in size from a known good one")
+
         # clear up the test file
         os.remove(test_plot_path)
+        os.remove(test_in_memory_plot_path)
 
     # check for exception for bad extension
     def test_plot_save_mfcc_color_boxed_fail_wrong_extension_exception(self):
@@ -64,8 +91,9 @@ class TestMfccPlotting(unittest.TestCase):
     def test_plot_save_mfcc_color_boxed_fail_wrong_extension_file_exists(self):
         # check for side effects of failing saving the file due to wrong extension
         file_path = None
+        file_bytesIO = None
         try:
-            file_path = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH,
+            file_path, file_bytesIO = plot_save_mfcc_color_boxes(self.AUDIO_1_PATH,
                                                    self.DIRECTORY_TEST,
                                                    "test_plot_mfcc_wav", "wav")
         except ValueError:
@@ -80,6 +108,9 @@ class TestMfccPlotting(unittest.TestCase):
             test_plot_path = f"{self.DIRECTORY_TEST}/test_plot_mfcc_wav.wav"
             self.assertFalse(Path(test_plot_path).is_file(), f"A file was created,"
                             "or exists of {test_plot_path} -> plot with wav extension!")
+
+            # check if No BytesIO object was returned
+            self.assertIs(file_bytesIO, None, msg="Plot's bytesIO was returned when it should be empty")
 
 
 
