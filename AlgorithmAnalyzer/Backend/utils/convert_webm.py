d7cdf9f2d101d01b2d43fc987d4bea9e6fa27345
MikolajBalcerek <mpbalcerek+github@gmail.com>
2018-12-20 22:00:34 +0100

    Change convert_webm to operate on BytesIO files
    on both input and output
    necessated by the move to DB solution

--- AlgorithmAnalyzer/Backend/utils/convert_webm.py
+++ AlgorithmAnalyzer/Backend/utils/convert_webm.py
@@ -1,19 +1,20 @@
 from pydub import AudioSegment
+from io import BytesIO
 
 
-def convert_webm_to_format(source_path: str, destination_path: str,
-                           format : str = "wav"):
+def convert_webm_to_format(source_bytesIO: BytesIO, format : str = "wav"):
     """ Helper function converting webm to wav so it can be handled
    by mainstream libraries
 
-   :param source_path: str path to source
-   :param destination_path: str path to save to without format
+   :param source_bytesIO: BytesIO object of source file
    :param format: intended format without ., e.g. wav
    """
-    export_path = destination_path+"."+format
+    export_file_bytesIO_in_memory = BytesIO()
+
     sound = AudioSegment.from_file(
-        source_path,
+        source_bytesIO,
         codec="opus"
-    ).export(export_path, format=format)
-    return export_path
+    ).export(export_file_bytesIO_in_memory, format=format)
+
+    return export_file_bytesIO_in_memory
 
