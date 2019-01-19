1637e4541deb60a38f8dcb99f013dca6aba3919c
Stanislaw-Golebiewski <slowik66@o2.pl>
2019-01-19 23:16:29 +0100

    populate tags in make

--- AlgorithmAnalyzer/Backend/populate_tags.py
+++ AlgorithmAnalyzer/Backend/populate_tags.py
@@ -6,10 +6,16 @@ sm = app.config['SAMPLE_MANAGER']
 
 tags = {
     "age": ["< 20", "20 - 40", "40 - 60", "> 60"],
-    "gender": ["male", "female"]
+    "gender": ["male", "female"],
+    "language": ["polish", "english", "german"]
     }
 
 print("populate logs...")
 for tag_name in tags:
-    sm.add_tag(tag_name, tags[tag_name])
+    try:
+        sm.add_tag(tag_name, tags[tag_name])
+        print(f"added tag {tag_name}")
+    except ValueError as e:
+        print(str(e))
+        continue
 print("fin...")
