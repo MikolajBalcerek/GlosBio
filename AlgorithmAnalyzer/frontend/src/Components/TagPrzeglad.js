1d397f4119918368692716d91b41637f211a2d4a
MikolajSzumigalski <m.szumigalski@op.pl>
2019-01-20 23:02:15 +0100

    nagaj i testuj

--- AlgorithmAnalyzer/frontend/src/Components/TagPrzeglad.js
+++ AlgorithmAnalyzer/frontend/src/Components/TagPrzeglad.js
@@ -6,8 +6,6 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
---
+++ AlgorithmAnalyzer/frontend/src/Components/TestModal.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Button from '@material-ui/core/Button'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+export default class TestModal extends Component {
+    render(){
+        return(
+            <Dialog 
+            open={this.props.testModalOpen} 
+            maxWidth='lg' 
+            style={{
+                margin: 20
+                }}>
+                <DialogTitle id="simple-dialog-title">Wyniki testu</DialogTitle>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <div 
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            width:'100%',
+                            alignItems: 'center'
+                            }}>
+                        </div>
+                    </div>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell style={{textAlign: 'center'}}>Prognozowany wynik:</TableCell>
+                                <TableCell style={{textAlign: 'center'}}>{this.props.predictions&&this.props.predictions.prediction ? 'UŻYTKOWNIK POTWIERDZONY' : 'UŻYTKOWNIK ODRZUCONY'  }</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                    <Table>
+                        <TableBody>
+                            {this.props.predictions && this.props.predictions.meta.length > 0 &&this.props.predictions.meta.map(
+                                key=>
+                                <TableRow>
+                                    <TableCell style={{textAlign: 'center'}}>{key}</TableCell>
+                                    <TableCell style={{textAlign: 'center'}}>{this.props.predictions.meta[key]}</TableCell>
+                                </TableRow>
+                                     )}
+                        </TableBody>
+                    </Table>
+                <Button variant="contained" onClick={()=>this.props.handleTestChange()} style={{margin: 30}}>
+                    Wyjdź
+                </Button>
+          </Dialog> 
+        )
+    }
+}
